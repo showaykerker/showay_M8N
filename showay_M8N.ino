b090f070109edea0a4ed90ea8ebdb9d08a2c4535
showay <df405102@gmail.com>
2019-01-29 11:12:42 +0800

    Mod main

--- showay_M8N.ino
+++ showay_M8N.ino
@@ -1,8 +1,8 @@
+#include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 #include <Wire.h>
-#include "Adafruit_Sensor.h"
-#include "Adafruit_HMC5883_U.h"
-#include "TinyGPSPlus.h"
+#include <Adafruit_Sensor.h>
+#include <Adafruit_HMC5883_U.h>
 
 /*
    This sample sketch demonstrates the normal use of a TinyGPS++ (TinyGPSPlus) object.
@@ -13,129 +13,59 @@
 
 static const int RXPin = 8, TXPin = 9;
 static const uint32_t GPSBaud = 9600;
+const int decimals = 8;
 
-Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345); // Assign a Uniquej ID to the HMC5883 Compass Sensor
-TinyGPSPlus gps; // The TinyGPS++ object
-SoftwareSerial ss(RXPin, TXPin); // The serial connection to the NEO-6m GPS module
+// Assign a Uniquej ID to the HMC5883 Compass Sensor
+Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+// The serial connection to the NEO-6m GPS module
+SoftwareSerial ss(RXPin, TXPin);
 
 
-void setup()
-{
+void setup(){
   Serial.begin(9600);
   ss.begin(GPSBaud);
-  
-  /*
-  Serial.println(F("Simple Test with TinyGPS++ and attached NEO-6M GPS module"));
-  Serial.print(F("Testing TinyGPS++ library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
-  Serial.println();
-  displaySensorDetails();
-  */
 }
 
-void loop()
-{
-  float lat=0, lng=0, heading=0;
-
-  if (gps.location.isValid()){
-     lat = gps.location.lat();
-     lng = gps.location.lng();
-  }
-  Serial.print(lat, 8); Serial.print(',');
-  Serial.print(lng, 8); Serial.print(',');
-  
-  float headingDegrees = get_headingDegrees();
-  Serial.println(heading, 6);
-  
-}
-
-void displaySensorDetails(void)
-{
-  sensor_t sensor;
-  mag.getSensor(&sensor);
-  Serial.println("------------------------------------");
-  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
-  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
-  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
-  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" uT");
-  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" uT");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" uT");  
-  Serial.println("------------------------------------");
-  Serial.println("");
-  delay(500);
-}
-
-void loop_not_in_use()
-{
-  // This sketch displays information every time a new sentence is correctly encoded from the GPS Module.
+void loop(){
   while (ss.available() > 0)
     if (gps.encode(ss.read()))
       displayGpsInfo();
 }
 
-void displayGpsInfo()
-{
-  // Prints the location if lat-lng information was recieved
+void displayGpsInfo(){
   Serial.print(F("Location: ")); 
-  if (gps.location.isValid())
-  {
-    Serial.print(gps.location.lat(), 6);
+  if (gps.location.isValid()){
+    Serial.print(gps.location.lat(), decimals);
     Serial.print(F(","));
-    Serial.print(gps.location.lng(), 6);
+    Serial.print(gps.location.lng(), decimals);
+    Serial.print(F(","));
   }
-  // prints invalid if no information was recieved in regards to location.
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
-
-  Serial.print(F("  Date/Time: "));
-  // prints the recieved GPS module date if it was decoded in a valid response.
-  if (gps.date.isValid())
-  {
-    Serial.print(gps.date.month());
-    Serial.print(F("/"));
-    Serial.print(gps.date.day());
-    Serial.print(F("/"));
-    Serial.print(gps.date.year());
-  }
-  else
-  {
-    // prints invalid otherwise.
-    Serial.print(F("INVALID"));
+  else{
+	Serial.print(0, decimals);
+    Serial.print(F(","));
+	Serial.print(0, decimals);
+    Serial.print(F(","));
   }
 
-  Serial.print(F(" "));
-  // prints the recieved GPS module time if it was decoded in a valid response.
-  if (gps.time.isValid())
-  {
-    if (gps.time.hour() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.hour());
-    Serial.print(F(":"));
-    if (gps.time.minute() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.minute());
-    Serial.print(F(":"));
-    if (gps.time.second() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.second());
-    Serial.print(F("."));
-    if (gps.time.centisecond() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.centisecond());
+  if(mag.begin()){
+    float heading = displayCompassInfo();
+	Serial.println(heading, decimals);
   }
-  else
-  {
-    // Print invalid otherwise.
-    Serial.print(F("INVALID"));
+  else{
+	Serial.println(0, decimals);
   }
-  Serial.println();
-  if(mag.begin())
-  {
-    displayCompassInfo();
-  }
+  delay(500);
 }
 
-float get_headingDegrees(){
+float displayCompassInfo(){
+  /* Get a new sensor event */ 
   sensors_event_t event; 
   mag.getEvent(&event);
-  
+ 
   // Hold the module so that Z is pointing 'up' and you can measure the heading with x&y
   // Calculate heading when the magnetometer is level, then correct for signs of axis.
   float heading = atan2(event.magnetic.y, event.magnetic.x);
@@ -148,12 +78,10 @@ float get_headingDegrees(){
   heading += declinationAngle;
   
   // Correct for when signs are reversed.
-  if(heading < 0)
-    heading += 2*PI;
+  if(heading < 0) heading += 2*PI;
     
   // Check for wrap due to addition of declination.
-  if(heading > 2*PI)
-    heading -= 2*PI;
+  if(heading > 2*PI) heading -= 2*PI;
    
   // Convert radians to degrees for readability.
   float headingDegrees = heading * 180/M_PI; 
@@ -161,20 +89,3 @@ float get_headingDegrees(){
   return headingDegrees;
 }
 
-void displayCompassInfo()
-{
-  /* Get a new sensor event */ 
-  //sensors_event_t event; 
-  //mag.getEvent(&event);
- 
-  /* Display the results (magnetic vector values are in micro-Tesla (uT)) */
-  //Serial.print("X: "); Serial.print(event.magnetic.x); Serial.print("  ");
-  //Serial.print("Y: "); Serial.print(event.magnetic.y); Serial.print("  ");
-  //Serial.print("Z: "); Serial.print(event.magnetic.z); Serial.print("  ");Serial.println("uT");
-
-  float headingDegrees = get_headingDegrees();
-  
-  Serial.print("Heading (degrees): "); Serial.println(headingDegrees);
-  
-  delay(500);
-}
